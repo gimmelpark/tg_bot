3672127cc452717b97e2d4f2f7d8e40f5278b54f
gimmelpark <dmitriy_kochetov@list.ru>
2021-11-26 23:14:32 +0300

    last fix

--- src/scripts/cbHandlers/addPlayer.js
+++ src/scripts/cbHandlers/addPlayer.js
@@ -1,6 +1,12 @@
 const messages = require("../../assets/messages.constant.js");
 const $f = require("../shared.js");
 
+const getPlayerUsername = (player) => {
+  if (player.username) return player.username
+  if (player.first_name) return player.first_name
+  return `id ${player.id}`
+}
+
 const handler = async function(msg, bot, data) {
   if (data.gameData.status === 'prepare') {
     const options = {
@@ -12,7 +18,7 @@ const handler = async function(msg, bot, data) {
     if (data.gameData.players.every(el => el.id !== msg.from.id)) {
       const newPlayer = {
         id: msg.from.id,
-        username: msg.from.username ? msg.from.username : msg.from.id,
+        username: getPlayerUsername(msg.from),
         choice: null,
       }
       data.gameData.players.push(newPlayer)
@@ -23,7 +29,7 @@ const handler = async function(msg, bot, data) {
       )
       await bot.sendMessage(msg.message.chat.id, `Добавлен игрок ${newPlayer.username}`)
     } else {
-      await bot.sendMessage(msg.message.chat.id, `Игрок ${msg.from.username ? msg.from.username : msg.from.id} уже в игре`)
+      await bot.sendMessage(msg.message.chat.id, `Игрок ${getPlayerUsername(msg.from)} уже в игре`)
     }
   }
 }
